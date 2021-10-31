fdd21f8da5cb4a4528752b357a78c16de022ee15
Nick Risaro <nrisaro@gmail.com>
2021-10-31 17:25:50 -0300

    Permite agregar cosas al freezer

--- freezer/freezer.go
+++ freezer/freezer.go
@@ -1,17 +1,75 @@
 package freezer
 
+import "fmt"
+
+// Freezer guarda productos y los mantiene refrigerados
+// Se pueden poner y sacar cosas en él y puedo fijarme que hay adentro
 type Freezer interface {
-	Productos() []interface{}
+	// Productos me dice que cosas tengo freezadas
+	Productos() []*Producto
+	// Agregar me deja meter algo en el freezer
+	Agregar(*Producto)
 }
 
+// FreezerEfimero guarda las cosas pero por un ratito no más
 type FreezerEfímero struct {
-
+	productos []*Producto
 }
 
+// NewFreezerEfímero construye un freezer efímero
 func NewFreezerEfímero() Freezer {
-	return new(FreezerEfímero)
+	productos := make([]*Producto, 0)
+	return &FreezerEfímero{productos: productos}
 }
 
-func (f *FreezerEfímero) Productos() []interface{} {
-	return make([]interface{}, 0)
+func (f *FreezerEfímero) Productos() []*Producto {
+	return f.productos
+}
+
+func (f *FreezerEfímero) Agregar(producto *Producto) {
+	f.productos = append(f.productos, producto)
+}
+
+// Medida unidad de medida de los productos que guardo en el Freezer
+type Medida int
+
+const (
+	Otra Medida = iota
+	Unidad
+	Kilo
+	Gramo
+	Litro
+	Mililitro
+)
+
+func (s Medida) String() string {
+	switch s {
+	case Unidad:
+		return "unidad(es)"
+	case Kilo:
+		return "kilo(s)"
+	case Gramo:
+		return "gramo(s)"
+	case Litro:
+		return "litro(s)"
+	case Mililitro:
+		return "mililitro(s)"
+	}
+	return ""
+}
+
+// Producto algo que se puede freezar
+type Producto struct {
+	nombre         string
+	cantidad       float64
+	unidadDeMedida Medida
+}
+
+// NewProducto construye un nuevo producto para guardarlo en el freezer
+func NewProducto(nombre string, cantidad float64, unidadDeMedida Medida) *Producto {
+	return &Producto{nombre: nombre, cantidad: cantidad, unidadDeMedida: unidadDeMedida}
+}
+
+func (p *Producto) String() string {
+	return fmt.Sprintf("%s: %0.2f %s", p.nombre, p.cantidad, p.unidadDeMedida)
 }
--- freezer/freezer_test.go
+++ freezer/freezer_test.go
@@ -10,5 +10,14 @@ import (
 func TestInicialmenteElFreezerEstáVacío(t *testing.T) {
 	miFreezer := freezer.NewFreezerEfímero()
 
-	assert.Equal(t, make([]interface{}, 0), miFreezer.Productos(), "Esperaba que el freezer esté vacío")
+	assert.Equal(t, []*freezer.Producto{}, miFreezer.Productos(), "Esperaba que el freezer esté vacío")
+}
+
+func TestPuedoAgregarUnaUnidadDeUnProducto(t *testing.T) {
+	miFreezer := freezer.NewFreezerEfímero()
+	miProducto := freezer.NewProducto("Pizza", 1.0, freezer.Unidad)
+
+	miFreezer.Agregar(miProducto)
+
+	assert.Equal(t, []*freezer.Producto{miProducto}, miFreezer.Productos(), "Esperaba que el freezer tenga una pizza")
 }

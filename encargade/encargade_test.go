7284a906ede4a42fce0e74994af108c18b74dd5b
Nick Risaro <nrisaro@gmail.com>
2021-11-22 17:26:29 -0300

    Segunda pasada para que le encargade guarde en la DB

--- encargade/encargade_test.go
+++ encargade/encargade_test.go
@@ -15,6 +15,7 @@ const conexiónALaBase = "file::memory:?cache=shared"
 type EncargadeTestSuite struct {
 	suite.Suite
 	encargadeConBaseDeDatos *encargade.Encargade
+	miFreezer               *freezer.Freezer
 }
 
 func (suite *EncargadeTestSuite) SetupTest() {
@@ -27,30 +28,32 @@ func (suite *EncargadeTestSuite) SetupTest() {
 	suite.encargadeConBaseDeDatos = encargade.NewEncargadeConBaseDeDatos(db)
 
 	suite.NotNil(suite.encargadeConBaseDeDatos, "Le encargade no debería ser nil")
+
+	suite.miFreezer = freezer.NewFreezer(1, "Un Freezer")
+	result := db.Create(suite.miFreezer)
+	suite.NoError(result.Error, "Debería haber creado el freezer")
+	suite.NotNil(suite.miFreezer.ID, "El freezer debería tener ID")
 }
 
 func (suite *EncargadeTestSuite) TestSiNoHayNadaEnElFreezerLeEncargadeSabeQueEstáVacío() {
-	miFreezer := freezer.NewFreezer(1, "Un Freezer")
-	encargade := encargade.NewEncargade(miFreezer)
+	encargade := encargade.NewEncargade(suite.miFreezer)
 
 	suite.Equal("El freezer está vacío", encargade.QueCosasHayEnElFreezer(), "Esperaba que el freezer esté vacío")
 }
 
 func (suite *EncargadeTestSuite) TestSiLeDigoALeEncargadeQueAgregueUnaPizzaLaAgrega() {
-	miFreezer := freezer.NewFreezer(1, "Un Freezer")
-	encargade := encargade.NewEncargade(miFreezer)
+	encargade := encargade.NewEncargade(suite.miFreezer)
 
 	err := encargade.Meter("Pizza, 1, unidad")
 
 	suite.Nil(err, "No esperaba un error")
 
 	miProducto := freezer.NewProducto("Pizza", 1.0, freezer.Unidad)
-	suite.Equal([]*freezer.Producto{miProducto}, miFreezer.Productos, "Esperaba que haya una pizza")
+	suite.Equal([]*freezer.Producto{miProducto}, suite.miFreezer.Productos, "Esperaba que haya una pizza")
 }
 
 func (suite *EncargadeTestSuite) TestSiHayUnaPizzaEnElFreezerLeEncargadeMeLoDice() {
-	miFreezer := freezer.NewFreezer(1, "Un Freezer")
-	encargade := encargade.NewEncargade(miFreezer)
+	encargade := encargade.NewEncargade(suite.miFreezer)
 	encargade.Meter("Pizza, 1, unidad")
 
 	suite.Equal("El freezer tiene:\n\n- Pizza: 1.00 unidad(es)\n", encargade.QueCosasHayEnElFreezer(), "Esperaba que le encargade me diga que hay una pizza")

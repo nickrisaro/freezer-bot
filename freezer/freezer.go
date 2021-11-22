8d72c1e4d28101b2cdf48517b63b3bc6e0678187
Nick Risaro <nrisaro@gmail.com>
2021-11-22 16:28:58 -0300

    El freezer ahora tiene nombre e identificador

--- freezer/freezer.go
+++ freezer/freezer.go
@@ -5,6 +5,10 @@ import "fmt"
 // Freezer guarda productos y los mantiene refrigerados
 // Se pueden poner y sacar cosas en él y puedo fijarme que hay adentro
 type Freezer interface {
+	// Identificador es un número único para distinguir este freezer de otros
+	Identificador() int64
+	// Nombre para saber como se llama este freezer
+	Nombre() string
 	// Productos me dice que cosas tengo freezadas
 	Productos() []*Producto
 	// Agregar me deja meter algo en el freezer
@@ -15,13 +19,23 @@ type Freezer interface {
 
 // FreezerEfimero guarda las cosas pero por un ratito no más
 type FreezerEfímero struct {
+	identificador int64
+	nombre string
 	productos []*Producto
 }
 
 // NewFreezerEfímero construye un freezer efímero
-func NewFreezerEfímero() Freezer {
+func NewFreezerEfímero(identificador int64, nombre string) Freezer {
 	productos := make([]*Producto, 0)
-	return &FreezerEfímero{productos: productos}
+	return &FreezerEfímero{identificador: identificador, nombre: nombre, productos: productos}
+}
+
+func (f *FreezerEfímero) Identificador() int64 {
+	return f.identificador
+}
+
+func (f *FreezerEfímero) Nombre() string {
+	return f.nombre
 }
 
 func (f *FreezerEfímero) Productos() []*Producto {
--- freezer/freezer_test.go
+++ freezer/freezer_test.go
@@ -7,14 +7,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestElFreezerTieneIdentificadorYNombre(t *testing.T) {
+	var identificador int64 = 1
+	nombre := "El freezer de Nick"
+	miFreezer := freezer.NewFreezerEfímero(identificador, nombre)
+
+	assert.Equal(t, identificador, miFreezer.Identificador(), "El identificador no es el esperado")
+	assert.Equal(t, nombre, miFreezer.Nombre(), "El nombre no es el esperado")
+}
+
 func TestInicialmenteElFreezerEstáVacío(t *testing.T) {
-	miFreezer := freezer.NewFreezerEfímero()
+	miFreezer := freezer.NewFreezerEfímero(1, "Un Freezer")
 
 	assert.Equal(t, []*freezer.Producto{}, miFreezer.Productos(), "Esperaba que el freezer esté vacío")
 }
 
 func TestPuedoAgregarUnaUnidadDeUnProducto(t *testing.T) {
-	miFreezer := freezer.NewFreezerEfímero()
+	miFreezer := freezer.NewFreezerEfímero(1, "Un Freezer")
 	miProducto := freezer.NewProducto("Pizza", 1.0, freezer.Unidad)
 
 	miFreezer.Agregar(miProducto)
@@ -23,7 +32,7 @@ func TestPuedoAgregarUnaUnidadDeUnProducto(t *testing.T) {
 }
 
 func TestSePuedeSacarUnProductoDelFreezer(t *testing.T) {
-	miFreezer := freezer.NewFreezerEfímero()
+	miFreezer := freezer.NewFreezerEfímero(1, "Un Freezer")
 	miProducto := freezer.NewProducto("Pizza", 1.0, freezer.Unidad)
 	miFreezer.Agregar(miProducto)
 
